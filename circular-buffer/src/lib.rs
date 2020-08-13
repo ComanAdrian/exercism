76914efd3b30055abe731dc4f11d2f232e7b13b0
ComanAdrian <adrian92coman@gmail.com>
2020-08-14 00:35:46 +0300

    Add circular-buffer (#1)

--- circular-buffer/src/lib.rs
+++ circular-buffer/src/lib.rs
@@ -1,13 +1,9 @@
 use std::fmt::Debug;
 
-pub struct CircularBuffer<T>
-where
-    T: Debug + Copy,
-{
-    capacity: usize,
-    oldest: usize,
+#[derive(Debug)]
+pub struct CircularBuffer<T> {
     head: usize,
-    values: Vec<T>,
+    values: Vec<Option<T>>,
 }
 
 #[derive(Debug, PartialEq)]
@@ -18,40 +14,62 @@ pub enum Error {
 
 impl<T> CircularBuffer<T>
 where
-    T: Debug + Copy,
+    T: Debug,
 {
     pub fn new(capacity: usize) -> Self {
-        CircularBuffer {
-            capacity,
-            oldest: 0,
-            head: 0,
-            values: Vec::with_capacity(3) as Vec<T>,
-        }
+        let values: Vec<Option<T>> = (0..capacity).map(|_| None).collect();
+        CircularBuffer { head: 0, values }
     }
 
     pub fn write(&mut self, element: T) -> Result<(), Error> {
-        println!("write: {:?}", self.values.len());
-        self.head += 1;
-        self.head %= self.capacity;
-        println!("write: {:?}", self.values);
-        Ok(self.values.push(element))
+        if self.values.get(self.head).unwrap().is_some() {
+            return Err(Error::FullBuffer);
+        } else {
+            *self.values.get_mut(self.head).unwrap() = Some(element);
+        }
+
+        self.update_head();
+        Ok(())
     }
 
     pub fn read(&mut self) -> Result<T, Error> {
-        let value = *self.values.get(self.oldest).unwrap();
-        self.values.remove(self.oldest);
-        self.oldest += 1;
-        self.oldest %= self.capacity;
-        println!("read: {:?}", self.values);
-        Ok(value)
-        // unimplemented!("Read the oldest element from the CircularBuffer or return EmptyBuffer error if CircularBuffer is empty.");
+        for i in 0..self.capacity() {
+            let mut index_to_check = self.head + i;
+            index_to_check = if index_to_check < self.capacity() {
+                index_to_check
+            } else {
+                index_to_check - self.capacity()
+            };
+
+            let elem = self.values.get_mut(index_to_check).unwrap();
+            if elem.is_some() {
+                let removed_element = elem.take().unwrap();
+                return Ok(removed_element);
+            }
+        }
+        Err(Error::EmptyBuffer)
     }
 
     pub fn clear(&mut self) {
-        unimplemented!("Clear the CircularBuffer.");
+        self.values.iter_mut().for_each(|elem| {
+            elem.take();
+        });
     }
 
-    pub fn overwrite(&mut self, _element: T) {
-        unimplemented!("Write the passed element to the CircularBuffer, overwriting the existing elements if CircularBuffer is full.");
+    pub fn overwrite(&mut self, element: T) {
+        *self.values.get_mut(self.head).unwrap() = Some(element);
+        self.update_head();
+    }
+
+    fn update_head(&mut self) {
+        self.head = if (self.head + 1) == self.capacity() {
+            0
+        } else {
+            self.head + 1
+        };
+    }
+
+    fn capacity(&self) -> usize {
+        self.values.len()
     }
 }
